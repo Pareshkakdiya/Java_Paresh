6341c343d195cafe509019ba35afe8327eb28497
Pareshkakdiya <110616772+Pareshkakdiya@users.noreply.github.com>
2023-01-18 08:57:22 +0530

    Add files via upload
    18Jan

---
+++ Remove.java
@@ -0,0 +1,19 @@
+package assignment;
+
+import java.util.ArrayList;
+
+public class Remove {
+public static void main(String[] args) {
+	ArrayList upList = new ArrayList();
+    upList.add("java");
+    upList.add("Orange");
+    upList.add("Black");
+    upList.add("White");
+    upList.add("Wooden");
+    
+    System.out.println(upList);
+    
+    upList.remove(2);
+    System.out.println(upList);
+}
+}
