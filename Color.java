2c24edde71e17d38028cb43afaf0549d897691c6
Pareshkakdiya <110616772+Pareshkakdiya@users.noreply.github.com>
2023-01-20 12:05:35 +0530

    Add files via upload
    20Jan

---
+++ Color.java
@@ -0,0 +1,35 @@
+package assignment;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Color {
+public static void main(String[] args) {
+	ArrayList<String> c1 = new ArrayList<>();
+	c1.add("Red");
+	c1.add("Green");
+	c1.add("Blue");
+	c1.add("Black");
+	c1.add("Purple");
+	System.out.println(c1);
+	
+	for (int i = 0; i < c1.size(); i++) {
+		System.out.println(c1.get(i));
+	
+	}
+	
+	c1.add(0,"Yellow");
+	System.out.println(c1);
+	
+	System.out.println("Element at position number 3 : "+c1.get(3));
+	
+	Iterator i1 = c1.iterator();
+	
+	while (i1.hasNext()) {
+		System.out.println(i1.next());
+		
+	}
+}
+}
+
