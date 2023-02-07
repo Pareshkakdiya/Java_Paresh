e201c359c33cf6f488482a29b730d566caa42868
Pareshkakdiya <110616772+Pareshkakdiya@users.noreply.github.com>
2023-02-07 10:16:00 +0530

    7Feb

---
+++ HashSetToArray.java
@@ -0,0 +1,16 @@
+package assignment;
+
+import java.util.HashSet;
+
+public class HashSetToArray {
+public static void main(String[] args) {
+	HashSet h1 = new HashSet();
+	h1.add(1);
+	h1.add(19.05);
+	h1.add("Paresh");
+	h1.add(32432.342);
+	h1.add("Kakadiya");
+	
+	System.out.println(h1);
+}
+}
